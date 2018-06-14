cec0cb51df639d148243783a34439a190510a1a6
NeoCat98 <00076017@uca.edu.sv>
2018-06-13 18:22:32 -0600

    se creo el main

---
+++ src/javiermejia/s/world/JavierMejiaSWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javiermejia.s.world;
+
+/**
+ *
+ * @author javie
+ */
+public class JavierMejiaSWorld {
+    public static void main(String[] args) {
+        Menu menu;
+        menu = Menu.getInstance();
+        menu.Inicio();
+    }
+}
